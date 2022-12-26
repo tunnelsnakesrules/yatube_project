943a2971b641ed12e45c4a9cf0364ecd569ea178
Фарух Темиров <alrov777@yandex.ru>
2022-12-26 18:16:42 +0300

    опысаны новые модели и адман зона

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,21 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
-# Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
-# Страница со списком мороженого
 def group_posts(request, slug):
-    return HttpResponse('Слаг работает')
+    group = get_object_or_404(Group, slug=slug)
+
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
